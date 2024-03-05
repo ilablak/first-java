e5dc398786be4b3dbcefdfb77c0789941739ffa7
Imanol Valero <imanol.valero@live.com>
2024-03-06 00:14:27 +0100

    feat: add another button, and its logic, to change from red to blue

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function changeColor() {
+function changeColorToBlue() {
     const square = document.getElementById('square')
     square.classList.remove('red')
     square.classList.add('blue')
 }
 
-const button = document.getElementById('button')
-button.onclick = changeColor
+const blue_button = document.getElementById('blue_button')
+blue_button.onclick = changeColorToBlue
+
+function changeColorToRed() {
+    const square = document.getElementById('square')
+    square.classList.remove('blue')
+    square.classList.add('red')
+}
+
+const red_button = document.getElementById('red_button')
+red_button.onclick = changeColorToRed
