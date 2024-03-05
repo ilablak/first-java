7376ed1adb14873fb56ae2450f5ee6a946c8bfc6
Imanol Valero <imanol.valero@live.com>
2024-03-06 00:16:35 +0100

    refactor: refactorize functions

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-function changeColorToBlue() {
+function changeColor(from_color, to_color) {
     const square = document.getElementById('square')
-    square.classList.remove('red')
-    square.classList.add('blue')
+    square.classList.remove(from_color)
+    square.classList.add(to_color)
+}
+
+function changeColorToBlue() {
+    changeColor('red', 'blue')
+}
+
+function changeColorToRed() {
+    changeColor('blue', 'red')
 }
 
 const blue_button = document.getElementById('blue_button')
 blue_button.onclick = changeColorToBlue
-
-function changeColorToRed() {
-    const square = document.getElementById('square')
-    square.classList.remove('blue')
-    square.classList.add('red')
-}
 
 const red_button = document.getElementById('red_button')
 red_button.onclick = changeColorToRed
