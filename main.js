0f10891fbc0e1299c4c034cc4f3783d949751f00
Imanol Valero <imanol.valero@live.com>
2024-03-06 00:23:55 +0100

    refactor: refactorize javascript functions

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
-function changeColor(from_color, to_color) {
+function changeColor(event) {
+    const button = event.target
+    const from_color = button.attributes.from_color.value
+    const to_color = button.attributes.to_color.value
+
     const square = document.getElementById('square')
     square.classList.remove(from_color)
     square.classList.add(to_color)
 }
 
-function changeColorToBlue(event) {
-    const button = event.target
-    const from_color = button.attributes.from_color.value
-    const to_color = button.attributes.to_color.value
-    changeColor(from_color, to_color)
-}
-
-function changeColorToRed(event) {
-    const button = event.target
-    const from_color = button.attributes.from_color.value
-    const to_color = button.attributes.to_color.value
-    changeColor(from_color, to_color)
-}
-
 const blue_button = document.getElementById('blue_button')
-blue_button.onclick = changeColorToBlue
+blue_button.onclick = changeColor
 
 const red_button = document.getElementById('red_button')
-red_button.onclick = changeColorToRed
+red_button.onclick = changeColor
