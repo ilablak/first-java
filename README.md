813d75d17d040744d55ad6a3db58516efb3bcb0e
imad <lablak.imad@gmail.com>
2024-03-07 13:04:33 +0100

    cambios primeros

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Fondo cambiante
 
 Este es un  ejemplo básico de cómo utilizar javascript para modificar el DOM
-
-Puedes ver [aquí](https://first-example-penascal-frontend-javascript-92056419f19817ca5682.gitlab.io/) la última versión funcionado online.
--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-function changeColor(color) {
+function changeColor(event) {
+    const button = event.target
+    const color = button.attributes.color.value
+
     const square = document.getElementById('square')
+    
     square.className = `square ${color}`
 }
 
-function colorChangeButtonOnClickHandler(event) {
-    const color = event.target.dataset.color
-    changeColor(color)
-}
 
-const buttons = document.querySelectorAll('[data-type=color_changing_button]')
-for (const button of buttons) {
-    button.onclick = colorChangeButtonOnClickHandler
-}
+const blue_button = document.getElementById('blue_button')
+blue_button.onclick = changeColor
+
+
+
+const red_button = document.getElementById('red_button')
+red_button.onclick = changeColor
+
+const green_button = document.getElementById('green_button')
+green_button.onclick = changeColor
